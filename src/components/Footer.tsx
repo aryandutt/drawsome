2d3a94f8ea38d153493716370f3159b134017bd2
aryandutt <aryandutt21@gmail.com>
2024-02-21 14:36:02 +0530

    bug: removes TS errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+// const Footer = () => {
+//   return (
+//     <div>Footer</div>
+//   )
+// }
 
-export default Footer
+// export default Footer
